345fc487e005dcbb3f4efe1b28598cad332a76da
Gyuwon <gyuwon@live.com>
2014-03-15 00:37:07 +0900

    MultiThreading

---
+++ Tasks/MultiThreading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 1000;
+
+            var t = new Thread(() => Write('+', count));
+            t.Start();
+
+            Write('-', count);
+
+            t.Join();
+        }
+
+        static void Write(char c, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
