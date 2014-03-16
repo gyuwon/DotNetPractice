918e755996778e9e5a055fade6d05251ef30cc5d
Gyuwon <gyuwon@live.com>
2014-03-16 12:56:14 +0900

    Batch vs. Event Driven

---
+++ IoC/Batch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number: ");
+            string input = Console.ReadLine();
+            double n;
+            if (double.TryParse(input, out n))
+            {
+                double square = n * n;
+                Console.WriteLine("Input: {0}", n);
+                Console.WriteLine("Square: {0}", square);
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+            Console.WriteLine("Press [enter] to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IoC/EventDriven/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using MahApps.Metro.Controls;
+
+namespace EventDriven
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnInputTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string s = this.Input.Text;
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                this.Square.Text = string.Empty;
+            }
+            else
+            {
+                double number;
+                if (double.TryParse(s, out number))
+                {
+                    double square = number * number;
+                    this.Square.Text = square.ToString();
+                }
+                else
+                {
+                    this.Square.Text = "Not a number";
+                }
+            }
+        }
+    }
+}
