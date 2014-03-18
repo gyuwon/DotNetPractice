d1254983feb03ce0e8a35b0913a0677b9ffe8d82
Gyuwon <gyuwon@live.com>
2014-03-18 18:01:00 +0900

    ContactManagerService

---
+++ IoC/ContactManager/ContactManager.Test/ContactsControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ContactManager.Controllers;
+using ContactManager.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ContactManager.Test
+{
+    [TestClass]
+    public class ContactsControllerTest
+    {
+        private Contact ironman = new Contact { Id = 1, FirstName = "Tony", LastName = "Stark", Email = "ironman@avengers.com" };
+        private Contact hulk = new Contact { Id = 2, FirstName = "Bruce", LastName = "Banner", Email = "hulk@avengers.com" };
+
+        [TestMethod]
+        public void Get_Should_Return_Contacts()
+        {
+            // Setup
+            var mock = new Mock<IRepository>();
+            var contacts = new List<Contact> { ironman, hulk };
+            mock.Setup(r => r.Get()).Returns(contacts.AsQueryable());
+            IRepository repository = mock.Object;
+            var controller = new ContactsController(repository);
+
+            // Exercise
+            var result = controller.Get().ToList();
+
+            // Verify
+            mock.Verify(r => r.Get(), Times.Once());
+            result.Should().NotBeNull();
+            result.Should().HaveCount(contacts.Count);
+            result[0].Should().Be(contacts[0]);
+            result[1].Should().Be(contacts[1]);
+        }
+
+        [TestMethod]
+        public void Post_Should_Call_Add_And_SaveChanges()
+        {
+            // Setup
+            var mock = new Mock<IRepository>();
+            var contact = ironman;
+            mock.Setup(r => r.Add(contact)).Verifiable();
+            mock.Setup(r => r.SaveChanges()).Verifiable();
+            IRepository repository = mock.Object;
+            var controller = new ContactsController(repository);
+
+            // Exercise
+            Contact result = controller.Post(contact);
+
+            // Verify
+            mock.VerifyAll();
+            result.Should().NotBeNull();
+            result.Should().Be(contact);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(HttpResponseException))]
+        public void Post_Should_Throw_500_Error_With_Duplicated_Email()
+        {
+            // Setup
+            var mock = new Mock<IRepository>();
+            var contact = ironman;
+            mock.Setup(r => r.Get()).Returns(new Contact[] { contact }.AsQueryable()).Verifiable();
+            IRepository repository = mock.Object;
+            var controller = new ContactsController(repository);
+
+            try
+            {
+                // Exercise
+                controller.Post(contact);
+            }
+            finally
+            {
+                // Verify
+                mock.VerifyAll();
+            }
+        }
+    }
+}
---
+++ IoC/ContactManager/ContactManagerService/NinjectDependencyResolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Web.Http.Dependencies;
+using Ninject;
+
+namespace ContactManager
+{
+    public class NinjectDependencyScope : IDependencyScope
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyScope(IKernel kernel)
+        {
+            Contract.Assert(kernel != null);
+
+            this._kernel = kernel;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this._kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this._kernel.GetAll(serviceType);
+        }
+
+        public void Dispose()
+        {
+            this._kernel = null;
+        }
+    }
+
+    public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+            : base(kernel)
+        {
+            this._kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(this._kernel);
+        }
+    }
+}
